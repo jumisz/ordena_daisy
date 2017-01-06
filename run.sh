0a2ec92831303ae27d432bfa2402296591f3f172
Juan M Salamanca <jumisz.dev@gmail.com>
2017-01-06 16:58:45 +0100

    Fixed run.sh bug

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Get the name of the last modified file.
 DOWNLOADS=$HOME/Downloads
-newest=$(ls -U $DOWNLOADS/*.zip| head -1)
+# Esperar 2 segundos por si el sistema no ha terminado de convertir la
+# descarga en ZIP
+sleep 2s
+echo "Ultimos archivos: $(ls -tU $DOWNLOADS/*.zip| head -3)"
+newest=$(ls -tU $DOWNLOADS/*.zip| head -1)
 "$(dirname $0)/ordena-daisy.sh" "$newest"
